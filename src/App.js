c0d7386625f2bf15e2cecce5fee92e40d3a293bc
Napster <rome.1205h@gmail.com>
2020-06-11 02:15:38 -0500

    add ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ListaHobbies from './componentes/vistas/ListaHobbies';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import tema from './tema/tema';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <MuiThemeProvider theme={tema}>
+        <ListaHobbies/>
+      </MuiThemeProvider>
+    )
+  }
 }
 
 export default App;
